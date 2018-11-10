95e29530bd796658367015b13c3b5e864faa606c
jordanrios94 <jordanrios.software@gmail.com>
2018-11-10 19:26:59 +0000

    feat: blog server up and running

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://jordan:batman13@ds151523.mlab.com:51523/blog_dev',
+  cookieKey: '123123123'
+};
